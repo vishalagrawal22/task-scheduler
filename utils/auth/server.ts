2c724a6f3e570d8c35808d08ec28b6ffb885fbcb
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-12-18 17:26:04 +0530

    Make getUserId throw unauthenticated error

--- utils/auth/server.ts
+++ utils/auth/server.ts
@@ -1,8 +1,13 @@
 import type { NextApiRequest } from "next";
 import { auth } from "../../firebase-config/server";
 
+export const UNAUTHENTICATED_ERROR = "unauthenticated";
+
 export async function getUserId(req: NextApiRequest) {
   const { authToken } = req.body;
+  if (!authToken) {
+    throw UNAUTHENTICATED_ERROR;
+  }
   const { uid } = await auth.verifyIdToken(authToken);
   return uid;
 }
