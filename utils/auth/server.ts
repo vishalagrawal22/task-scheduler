65a31e0255c11c2bb77c4845b961be965ca73f13
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-12-18 16:37:44 +0530

    Use request to get user id

--- utils/auth/server.ts
+++ utils/auth/server.ts
@@ -1,6 +1,8 @@
+import type { NextApiRequest } from "next";
 import { auth } from "../../firebase-config/server";
 
-export async function getUserId(authToken: string) {
+export async function getUserId(req: NextApiRequest) {
+  const { authToken } = req.body;
   const { uid } = await auth.verifyIdToken(authToken);
   return uid;
 }
