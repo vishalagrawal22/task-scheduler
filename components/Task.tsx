f559ef6c3cecb313d635f1a587ad16fa8e14fb19
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-12-20 13:57:21 +0530

    Add task component

---
+++ components/Task.tsx
@@ -0,0 +1,110 @@
+import { useState } from "react";
+import { DateTime } from "luxon";
+
+import { ITask } from "../utils/interfaces/Task";
+
+const MAX_SHORT_DESCRIPTION_LENGTH = 80;
+
+export function Task({ task }: { task: ITask }) {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [showFullDescription, setShowFullDescription] = useState(false);
+  const date = DateTime.fromISO(new Date(task.scheduledDate).toISOString());
+  const humanReadable = date.toLocaleString(DateTime.DATETIME_MED);
+
+  return (
+    <div className="flex flex-col w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <div className="flex flex-col items-end px-2 pt-2">
+        <button
+          id="dropdownButton"
+          data-dropdown-toggle="dropdown"
+          onClick={() => {
+            setDropdownOpen(!dropdownOpen);
+          }}
+          className="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5"
+          type="button"
+        >
+          <span className="sr-only">Toggle dropdown</span>
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path>
+          </svg>
+        </button>
+        {dropdownOpen && (
+          <div
+            id="dropdown"
+            className="absolute mt-10 z-10 text-base list-none bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700"
+          >
+            <ul className="py-1" aria-labelledby="dropdownButton">
+              <li>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                >
+                  Edit
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                >
+                  Delete
+                </a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+
+      <div className="flex flex-col justify-between flex-grow items-center pb-6">
+        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+          {task.title}
+        </h5>
+        <span className="mx-6 mb-3 text-sm text-gray-500 dark:text-gray-400">
+          {humanReadable}
+        </span>
+        {task.description && (
+          <span className="mx-6 text-sm text-gray-700 dark:text-gray-600">
+            {showFullDescription ? (
+              task.description
+            ) : (
+              <>
+                {task.description.substring(0, MAX_SHORT_DESCRIPTION_LENGTH)}
+                {task.description.length > MAX_SHORT_DESCRIPTION_LENGTH && (
+                  <>
+                    ...
+                    <span
+                      className="text-blue-600 hover:text-blue-800"
+                      onClick={() => setShowFullDescription(true)}
+                    >
+                      {" "}
+                      read more
+                    </span>
+                  </>
+                )}
+              </>
+            )}
+          </span>
+        )}
+        {!task.completed ? (
+          <div className="flex mt-4 space-x-3 md:mt-6">
+            <span className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Mark as complete
+            </span>
+          </div>
+        ) : (
+          <div className="flex mt-4 space-x-3 md:mt-6">
+            <span className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-gray-600 rounded-lg hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Mark as incomplete
+            </span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
--- utils/interfaces/Task.ts
+++ utils/interfaces/Task.ts
@@ -1,4 +1,5 @@
 export interface ITask {
+  _id: keyof ITask;
   title: String;
   description?: String;
   createdBy: String;
