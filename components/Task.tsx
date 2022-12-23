906a95aa3eadf62ff7504f715b53eadb3455cef8
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-12-23 18:32:56 +0530

    Remove dark mode styles for consistency

--- components/Task.tsx
+++ components/Task.tsx
@@ -12,7 +12,7 @@ export function Task({ task }: { task: ITask }) {
   const humanReadable = date.toLocaleString(DateTime.DATETIME_MED);
 
   return (
-    <div className="flex flex-col w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+    <div className="flex flex-col w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-md">
       <div className="flex flex-col items-end px-2 pt-2">
         <button
           id="dropdownButton"
@@ -20,7 +20,7 @@ export function Task({ task }: { task: ITask }) {
           onClick={() => {
             setDropdownOpen(!dropdownOpen);
           }}
-          className="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5"
+          className="inline-block text-gray-500 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg text-sm p-1.5"
           type="button"
         >
           <span className="sr-only">Toggle dropdown</span>
@@ -37,13 +37,13 @@ export function Task({ task }: { task: ITask }) {
         {dropdownOpen && (
           <div
             id="dropdown"
-            className="absolute mt-10 z-10 text-base list-none bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700"
+            className="absolute mt-10 z-10 text-base list-none bg-white divide-y divide-gray-100 rounded shadow w-44"
           >
             <ul className="py-1" aria-labelledby="dropdownButton">
               <li>
                 <a
                   href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                 >
                   Edit
                 </a>
@@ -51,7 +51,7 @@ export function Task({ task }: { task: ITask }) {
               <li>
                 <a
                   href="#"
-                  className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                  className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100"
                 >
                   Delete
                 </a>
@@ -62,14 +62,10 @@ export function Task({ task }: { task: ITask }) {
       </div>
 
       <div className="flex flex-col justify-between flex-grow items-center pb-6">
-        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-          {task.title}
-        </h5>
-        <span className="mx-6 mb-3 text-sm text-gray-500 dark:text-gray-400">
-          {humanReadable}
-        </span>
+        <h5 className="mb-1 text-xl font-medium text-gray-900">{task.title}</h5>
+        <span className="mx-6 mb-3 text-sm text-gray-500">{humanReadable}</span>
         {task.description && (
-          <span className="mx-6 text-sm text-gray-700 dark:text-gray-600">
+          <span className="mx-6 text-sm text-gray-700">
             {showFullDescription ? (
               task.description
             ) : (
@@ -93,13 +89,13 @@ export function Task({ task }: { task: ITask }) {
         )}
         {!task.completed ? (
           <div className="flex mt-4 space-x-3 md:mt-6">
-            <span className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <span className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300">
               Mark as complete
             </span>
           </div>
         ) : (
           <div className="flex mt-4 space-x-3 md:mt-6">
-            <span className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-gray-600 rounded-lg hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <span className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-gray-600 rounded-lg hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300">
               Mark as incomplete
             </span>
           </div>
